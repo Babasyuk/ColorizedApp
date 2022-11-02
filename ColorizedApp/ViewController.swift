fc4d8c07299f3555b9aa39a379edcc7631100c95
Babasyuk <artem.babasyuk@mail.ru>
2022-11-02 18:10:52 +0300

    Setup methods

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupColorView()
+        setupRedSlider()
+        setupGreenSlider()
+        setupBlueSlider()
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func redSliderAction() {
+//        view.backgroundColor = view.backgroundColor?.withAlphaComponent(CGFloat(redSlider.value))
+        colorView.backgroundColor = colorView.backgroundColor?.withAlphaComponent(CGFloat(redSlider.value))
+    }
+    
+    
+    // MARK: - Private Methods
+    private func setupColorView() {
+        colorView.layer.cornerRadius = 10
+        
+    }
+    
+    private func setupRedSlider() {
+        redSlider.value = 0.05
+        redSlider.minimumTrackTintColor = .red
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 255
+    }
+    
+    private func setupGreenSlider() {
+        greenSlider.value = 0.27
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 255
+    }
+    
+    private func setupBlueSlider() {
+        blueSlider.value = 0.49
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 255
     }
 
 
